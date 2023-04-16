2cb55359da5386509bb68e2b8eca478f063b89bf
Mohammad Othman <emranm226@hotmail.com>
2023-04-16 13:19:10 +0100

    Add setup.py for package distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name="ChurnPrediction-E2E-ML-Pipeline",
+    name="churn_prediction",
     version="0.1.0",
+    author="Mohammad Othman",
+    description="A churn prediction project using E2E machine learning pipeline",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/churn_prediction",
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.9",
     install_requires=[
-        'pandas',
-        'numpy',
-        'scikit-learn',
-        'dill',
-        'xgboost'
+        "pandas",
+        "numpy",
+        "scikit-learn",
+        "dill",
+        "xgboost",
+        "Flask",
+        "gunicorn",
     ],
 )
